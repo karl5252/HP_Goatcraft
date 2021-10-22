452c94f9bb884ce1216efb36679c7f24ab55c840
karl <karl.michalik@gmail.com>
2021-10-22 23:28:00 +0200

    *add html code

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 
 # Create your tests here.
@@ -10,3 +11,12 @@ class SmokeTest(TestCase):
     def test_root_url_resolves_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Lista rzeczy do zrobienia</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
+
+
