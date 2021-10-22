e69562e609fb85815ca707eb98b0f477c5e33b9f
karl <karl.michalik@gmail.com>
2021-10-22 21:57:08 +0200

    *add unittest support to functional_test.py

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox(executable_path="C:\\Users\\karlm\\Documents\\Browser_Drivers\\geckodriver.exe")
-browser.get('http://localhost:8000')
-# naglowek zawiera slowo listy
-assert 'Listy' in browser.title, 'Browser title is: ' + browser.title
-# uzytkownik zostaje zachecony aby wpisac rzecz doz robienia
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox(executable_path="C:\\Users\\karlm\\Documents\\Browser_Drivers\\geckodriver.exe")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Uzytkownik dowiedzial sie o nowej aplikacji dostepnej pod adresem...
+        # U. otwiera strone glowna aplikacji...
+        self.browser.get('http://localhost:8000')
+
+        # user zwrocil uwage ze tytul i naglowek zawiera slowo listy
+        self.assertIn('Listy', self.browser.title)
+        self.fail('zakonczenie testu')
+        # uzytkownik zostaje zachecony aby wpisac rzecz doz robienia
 # w polu tekstoowym wpisal 'kupic pawie piora'
 # po nacisnieicu klawisza enter storna zostala uaktualniona i wyseietla
 # "1.kupic prawie piora"
@@ -19,4 +33,5 @@ assert 'Listy' in browser.title, 'Browser title is: ' + browser.title
 
 # pod podanym adresem zostaje wyswietlona lista reczy do zrobienia
 
-browser.quit()
+    if __name__ == '__main__':
+        unittest.main(warnings='ignore')
